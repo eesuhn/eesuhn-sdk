e38986525ad7d0f8c64bce7d09a46cbce181dc7b
eesuhn <eason.yihong@gmail.com>
2025-03-02 16:27:28 +0800

    Bump v0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_desc = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='eesuhn_sdk',
-    version='0.1.0',
+    version='0.1.1',
     description='eesuhn\'s Personal SDK',
     long_description=long_desc,
     long_description_content_type='text/markdown',
